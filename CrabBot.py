ff22e4646404313fa3e8c57c99a40d8bad9f45ad
Jeffrey <TAOTheCrab@gmail.com>
2016-04-08 00:09:56 -0700

    Add !sir, disable !annoy
    Also working on better command parsing, but committing working code for
    now

--- CrabBot.py
+++ CrabBot.py
@@ -10,7 +10,11 @@ import logging
 
 client = discord.Client()
 
+# TODO It would probably be nicer to store these somewhere else
 mobas = ["Smite", "SMNC", "Strife", "Overwatch", "HoN", "AoS", "HotS"]
+places = ["Desert Bus", "the woods", "a Discord server", "DotA", "a video game",
+            "a hall of Ricks", "an interdimensional peace zone", "the TARDIS"
+            "Twitch chat", "Dark Souls", "Stack Overflow"]
 
 logging.basicConfig(level=logging.INFO)
 
@@ -23,16 +27,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # TODO look into better startswith processing (@bot.command ?)
     if message.content.startswith('!takeover'):
         await client.send_message(message.channel, "Something something robots")
 
-    if message.content.startswith('!dota') or message.content.startswith('!lol'):
+    elif message.content.startswith('!dota') or message.content.startswith('!lol'):
         num = random.randint(1, (len(mobas) - 1))
         await client.send_message(message.channel, "This is a strange {} mod".format(mobas[num]))
 
-    if message.content.startswith('!annoy'):
+
+    #elif message.content.startswith('!annoy'):
         # TODO anti-spam cooldown (probably a good idea for most commands)
-        await client.send_message(message.channel, "Ho ho ha ha!", tts=True)
+        #await client.send_message(message.channel, "Ho ho ha ha!", tts=True)
+
+    elif message.content.startswith('!sir'):
+        # Yes, possibly having repeats is intentional, more fun that way
+        placeOne = random.randint(1, (len(places) - 1))
+        placeTwo = random.randint(1, (len(places) - 1))
+        reply = "Sir, this is {}, not {}.".format(places[placeOne], places[placeTwo])
+        await client.send_message(message.channel, reply)
 
 
 # IMPROVEMENT give option to use args instead of cfg file
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CrabBot
 Silly Discord bot for giggles and testing
 
+Mostly prints funny messages in reply to ! commands (see on_message code for commands)
+
 Requires [discord.py](https://github.com/Rapptz/discord.py) v0.10 (async beta)
 
 Needs a user.cfg in PWD (for now) with Discord credentials in the format:
