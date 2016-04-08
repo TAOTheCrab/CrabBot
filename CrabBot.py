329fcc92b6ea58db7842103e0417971e9b8b0eb2
Jeffrey <TAOTheCrab@gmail.com>
2016-04-08 01:12:16 -0700

    Redid bot commands to use discord.py ext.commands
    - Command prefix changed to dynamic (currently !crab)
    - Added thumbsup
    - Failed at version control

--- CrabBot.py
+++ CrabBot.py
@@ -1,52 +1,61 @@
 #!/usr/bin/env python3
+# NOTE: using Python3.5+ async syntax
 # Simple Discord chatbot for fun
 #
 # Requires user.cfg with valid username/password combo
 
 import discord
-import asyncio
+from discord.ext import commands
 import random
 import logging
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!crab', description="Huh, another bot")
 
 # TODO It would probably be nicer to store these somewhere else
 mobas = ["Smite", "SMNC", "Strife", "Overwatch", "HoN", "AoS", "HotS"]
 places = ["Desert Bus", "the woods", "a Discord server", "DotA", "a video game",
-            "a hall of Ricks", "an interdimensional peace zone", "the TARDIS"
+            "a hall of Ricks", "an interdimensional peace zone", "the TARDIS",
             "Twitch chat", "Dark Souls", "Stack Overflow"]
 
 logging.basicConfig(level=logging.INFO)
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
-async def on_message(message):
-    # TODO look into better startswith processing (@bot.command ?)
-    if message.content.startswith('!takeover'):
-        await client.send_message(message.channel, "Something something robots")
+@bot.command(help='The bots have something to say')
+async def takeover():
+    await bot.say("Something something robots")
 
-    elif message.content.startswith('!dota') or message.content.startswith('!lol'):
-        num = random.randint(1, (len(mobas) - 1))
-        await client.send_message(message.channel, "This is a strange {} mod".format(mobas[num]))
+@bot.command(help='Well, what is this Lords Management then?', aliases=['lol'])
+async def dota():
+    num = random.randint(1, (len(mobas) - 1))
+    await bot.say("This is a strange {} mod".format(mobas[num]))
 
+@bot.command(enabled=False, hidden=True)
+async def annoy():
+    # TODO anti-spam cooldown (probably a good idea for most commands)
+    # NOTE so far untested with bot.say version, unsure if tts works
+    await bot.say("Ho ho ha ha!", tts=True)
 
-    #elif message.content.startswith('!annoy'):
-        # TODO anti-spam cooldown (probably a good idea for most commands)
-        #await client.send_message(message.channel, "Ho ho ha ha!", tts=True)
+@bot.command(help='For the unruly patron')
+async def sir():
+    # Yes, possibly having repeats is intentional, more fun that way
+    placeOne = random.randint(1, (len(places) - 1))
+    placeTwo = random.randint(1, (len(places) - 1))
+    reply = "Sir, this is {}, not {}.".format(places[placeOne], places[placeTwo])
+    await bot.say(reply)
 
-    elif message.content.startswith('!sir'):
-        # Yes, possibly having repeats is intentional, more fun that way
-        placeOne = random.randint(1, (len(places) - 1))
-        placeTwo = random.randint(1, (len(places) - 1))
-        reply = "Sir, this is {}, not {}.".format(places[placeOne], places[placeTwo])
-        await client.send_message(message.channel, reply)
+@bot.command()
+async def assist():
+    await bot.say("Help is transient, and for some reason is not provided here.")
 
+@bot.command()
+async def thumbsup():
+    await bot.reply("👍")
 
 # IMPROVEMENT give option to use args instead of cfg file
 # if __name__ == "__main__":
@@ -60,4 +69,4 @@ user_config.close()
 username = username.strip('\n\r')
 password = password.strip('\n\r')
 
-client.run(username, password)
+bot.run(username, password)
